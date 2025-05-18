4d417357152b4bf0ceeb04d89d27025e24effddc
BILBODEVE <facundotaraujo@hotmail.com>
2025-05-18 13:18:33 -0300

    metodo cargarModal + quite los parametros de model

--- src/presentacion/controlador/ControladorGeneral.java
+++ src/presentacion/controlador/ControladorGeneral.java
@@ -1,27 +1,40 @@
 package presentacion.controlador;
 
 import presentancion.vista.VentanaPrincipal;
+
+import java.util.ArrayList;
 import javax.swing.DefaultListModel;
 import entidad.Persona;
+import negocio.PersonaNegocio;
+import negocioImpl.PersonaNegocioImpl;
 
 public class ControladorGeneral {
 
-    private VentanaPrincipal ventanaPrincipal;
-    private ControladorAgregar controladorAgregar;
-    private ControladorModificar controladorModificar;
-    private DefaultListModel<Persona> listaPersonas;
+	private VentanaPrincipal ventanaPrincipal;
+	private ControladorAgregar controladorAgregar;
+	private ControladorModificar controladorModificar;
+	private ControladorEliminar controladorEliminar;
 
-    public ControladorGeneral() {
-        ventanaPrincipal = new VentanaPrincipal();
-        listaPersonas = new DefaultListModel<>();
+	public ControladorGeneral() {
+		ventanaPrincipal = new VentanaPrincipal();
 
-        controladorAgregar = new ControladorAgregar(ventanaPrincipal, listaPersonas);
-        controladorModificar = new ControladorModificar(ventanaPrincipal, listaPersonas);
+		controladorAgregar = new ControladorAgregar(ventanaPrincipal);
+		controladorModificar = new ControladorModificar(ventanaPrincipal);
+		controladorEliminar = new ControladorEliminar(ventanaPrincipal);
 
-        inicializar();
-    }
+		inicializar();
+	}
 
-    public void inicializar() {
-        ventanaPrincipal.setVisible(true);
-    }
+	public void inicializar() {
+		ventanaPrincipal.setVisible(true);
+	}
+
+	public static void cargarModal(ArrayList<Persona> personas, DefaultListModel<Persona> modal) {
+		modal.removeAllElements();
+		if (personas != null) {
+			for (Persona persona : personas) {
+				modal.addElement(persona);
+			}
+		}
+	}
 }
